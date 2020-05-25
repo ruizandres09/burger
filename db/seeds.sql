b6e1e77adf732a50723c4e283c35df27495fe7c3
MXN <engineerandres@gmail.com>
2020-05-24 20:53:06 -0700

    i set up the folder/file structure

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
-INSERT INTO burgers (name) VALUES ('Meeses');
-INSERT INTO burgers (name) VALUES ('Bobbi');
-INSERT INTO burgers (name, devoured) VALUES ('Sylvester', true);
-INSERT INTO burgers (name, devoured) VALUES ('Marilyn Manson', true);
-INSERT INTO burgers (name, devoured) VALUES ('Joe Biden', true);
-INSERT INTO burgers (name) VALUES ('Owens');
+INSERT INTO burgers (burgername,devoured) VALUES ('cheese burger',false);
+INSERT INTO burgers (burgername,devoured) VALUES ('special burger',fasle);
+INSERT INTO burgers (burgername,devoured, ) VALUES ('western burger', true);
+INSERT INTO burgers (burgername,devoured, ) VALUES ('avacado burger', true);
+INSERT INTO burgers (burgername,devoured, ) VALUES ('jalapeno burger', true);
+INSERT INTO burgers (burgername,devoured) VALUES ('plain burger', fasle);
