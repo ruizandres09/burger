db708306cb3bf2b8d45896808ce3dc2cba02dae1
MXN <engineerandres@gmail.com>
2020-05-27 21:19:51 -0700

    having issues setting up the routes in the burger controller and not sure how to adapt the activity js to work for this burger app.

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -6,44 +6,14 @@ var router = express.Router();
 var burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
+//testing with simply "hello"
 router.get("/", function (req, res) {
-  cat.all(function (data) {
-    var hbsObject = {
-      cats: data,
-    };
-    console.log(hbsObject);
-    res.render("index", hbsObject);
-  });
+  res.render("Hello");
 });
-
-router.post("/api/cats", function (req, res) {
-  cat.create(["name", "sleepy"], [req.body.name, req.body.sleepy], function (
-    result
-  ) {
-    // Send back the ID of the new quote
-    res.json({ id: result.insertId });
-  });
-});
-
-router.put("/api/cats/:id", function (req, res) {
-  var condition = "id = " + req.params.id;
-
-  console.log("condition", condition);
-
-  cat.update(
-    {
-      sleepy: req.body.sleepy,
-    },
-    condition,
-    function (result) {
-      if (result.changedRows === 0) {
-        // If no rows were changed, then the ID must not exist, so 404
-        return res.status(404).end();
-      }
-      res.status(200).end();
-    }
-  );
-});
+//request for burgers
+// router.get("/burgers",function(req,res){
+//     burger.all(function(burgerstuff))
+// })
 
 // Export routes for server.js to use.
 module.exports = router;
